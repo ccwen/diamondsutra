df9500bab31a3322bf09a3d5720794738d1041ff
yapcheahshen <yapcheahshen@gmail.com>
2015-05-04 13:27:49 +0800

    add segname for dsl

---
+++ dedup.js
@@ -0,0 +1,18 @@
+var fs=require("fs");
+var dsl=fs.readFileSync("dsl_jwn.xml","utf8").split(/\r?\n/);
+var names={};
+
+dsl.map(function(line,idx){
+	line.replace(/<seg id=".+?"(.*?)\/>/g,function(m,m1){
+		if (m1) {
+			var m=m1.match(/name="(.+?)"/);
+			if (m) {
+				var name=m[1];
+				if (names[name]) {
+					console.log("repeat name '"+name+"' at line",idx+1)
+				}
+				names[name]=true;
+			}
+		}
+	});
+});
