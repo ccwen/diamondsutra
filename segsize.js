4590b0a9c48499c4f38093754cd10cede706a8d7
yapcheahshen <yapcheahshen@gmail.com>
2015-05-09 14:47:38 +0800

    use reduce

--- segsize.js
+++ segsize.js
@@ -3,20 +3,20 @@ var fn=process.argv[2]||"dsl_jwn.xml";
 
 var doSeg=function(id,text,sz){
 	sz.push([id,text.length]);
-	return text.length;
 }
-var totalsize=0, segcount=0;
+
 var doFile=function(content) {
 	var ex=0, pbid="_", sizes=[] ; 
 	content.replace(/<seg id=".+?" name="(.*?)"\/>/g,function(m,m1,at){
-		totalsize+=doSeg(pbid,content.substring(ex,at-1),sizes);
+		doSeg(pbid,content.substring(ex,at-1),sizes);
 		ex=at, pbid=m1;
 	});
-	totalsize+=doSeg(pbid,content.substring(ex),sizes); //don't forget to process last segment
+	doSeg(pbid,content.substring(ex),sizes); //don't forget to process last segment
 	return sizes;
 }
 
 var segsize=doFile(fs.readFileSync(fn,"utf8"));
 segsize.sort(function(a,b){return b[1]-a[1]});
 console.log(segsize);
+var totalsize=segsize.reduce(function(prev,seg){return seg[1]+prev} ,0);
 console.log("average",totalsize/segsize.length);
