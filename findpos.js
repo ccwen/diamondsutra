3e5a3906d42e523d1b95eea42784614fc8d95e9e
yapcheahshen <yapcheahshen@gmail.com>
2015-07-10 15:14:44 +0800

    add findpos.js

---
+++ findpos.js
@@ -0,0 +1,23 @@
+var fs=require("fs");
+var defs=require("./"+process.argv[2]);
+var jwn=fs.readFileSync("dsl_jwn.xml","utf8");
+
+jwn=jwn.replace(/<seg id="(.+?)" name="(.+?)"/g,function(m,m1,m2){
+	return "{"+m1+"_"+m2+"}";
+});
+
+jwn=jwn.replace(/<.+?>/g,"").replace(/\r?\n/g,"");
+
+var findDef=function(def) {
+	var idx=jwn.indexOf(def.text);
+	if (idx==-1)return;
+
+	var seg=jwn.substr(0,idx).lastIndexOf("{");
+	var segend=jwn.substr(0,idx).lastIndexOf("}");
+	def.seg=jwn.substring(seg+1,segend);
+	def.offset=idx-seg;
+	//console.log(def.name,idx)
+}
+
+defs.map(findDef);
+console.log(defs);
